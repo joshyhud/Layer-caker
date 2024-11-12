50d74b51ef767d9341107ea4b6c4529ee2929455
Josh Hudson <josh@bamboonine.co.uk>
2024-11-12 08:58:11 +0000

    amended for block content

--- src/app/(frontend)/posts/[slug]/page.tsx
+++ src/app/(frontend)/posts/[slug]/page.tsx
@@ -2,6 +2,8 @@ import { sanityFetch } from "@/sanity/lib/live";
 import { POST_QUERY } from "@/sanity/lib/queries";
 import { notFound } from "next/navigation";
 import Link from "next/link";
+import { PortableText } from "next-sanity";
+import { components } from "@/sanity/portableTextComponents";
 
 export default async function Page({
   params,
@@ -20,6 +22,11 @@ export default async function Page({
   return (
     <main className="container mx-auto grid grid-cols-1 gap-6 p-12">
       <h1 className="text-4xl font-bold text-balance">{post?.title}</h1>
+      {post?.body ? (
+        <div className="prose">
+          <PortableText value={post?.body} components={components} />
+        </div>
+      ) : null}
       <hr />
       <Link href="/posts">&larr; Return to index</Link>
     </main>
